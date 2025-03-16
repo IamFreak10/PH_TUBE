38a8be881cdcd82d93b0c1e9c853cd8ba682e16b
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2025-03-16 13:40:30 +0600

    Final Commit

--- script/index.js
+++ script/index.js
@@ -1,3 +1,42 @@
+const removeActiveButtons = () => {
+  const activeButtons = document.getElementsByClassName('active');
+  for (let button of activeButtons) {
+    button.classList.remove('active');
+  }
+};
+
+const loadVideoDetails = (videoId) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+
+const displayVideoDetails = (video) => {
+  const container = document.getElementById('details-container');
+  const modal = document.getElementById('video_details');
+
+  container.innerHTML = '';
+  const videoDetails = document.createElement('div');
+
+  videoDetails.innerHTML = `
+  <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+   
+  </div>
+</div>
+  `;
+  container.appendChild(videoDetails);
+  modal.showModal();
+};
+
 function loadCategories() {
   // 1----------------- fetch categories -----------------
   fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -9,15 +48,31 @@ function loadCategories() {
     });
 }
 
-function loadVideos() {
+function loadVideos(text = '') {
+  removeActiveButtons();
+  const allbtn = document.getElementById('all-btn');
+  allbtn.classList.add('active');
   // 1----------------- fetch videos -----------------
-  fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${text}`
+  )
     //2----------------- convert response to json -----------------
     .then((res) => res.json())
     //3-send data to displayVideos function
     .then((data) => displayVideos(data.videos));
 }
 
+const loadVideosByCategory = (categoryId) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${categoryId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActiveButtons();
+      const clickButton = document.getElementById(`btn-${categoryId}`);
+      clickButton.classList.add('active');
+      displayVideos(data.category);
+    });
+};
 /* {
   "category_id": "1001",
   "video_id": "aaaa",
@@ -49,8 +104,8 @@ function displayCategories(categories) {
     // create a div element
     const catagoryDiv = document.createElement('div');
     catagoryDiv.innerHTML = `
-  <button class="btn hover:bg-[#FF1F3D] hover:text-white ">${cat.category}</button>
-  `;
+    <button id="btn-${cat.category_id}" onclick="loadVideosByCategory(${cat.category_id})" class="btn hover:bg-[#FF1F3D] hover:text-white ">${cat.category}</button>
+    `;
     //append each category to the categories container
     categoriesContainer.appendChild(catagoryDiv);
   }
@@ -58,13 +113,26 @@ function displayCategories(categories) {
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById('video-container');
+  videoContainer.innerHTML = '';
+  if (videos.length === 0) {
+    videoContainer.innerHTML = `<div class="my-55 items-center col-span-full flex flex-col text-center gap-10">
+        <img class="w-[250px]" src="./assets/Icon.png" alt="" />
+        <h1 class="text-2xl font-bold">
+          Oops!! Sorry, There is no<br />
+          content here
+        </h1>
+      </div>`;
+    return;
+  }
   videos.forEach((video) => {
     // Create a div element
     const videoDiv = document.createElement('div');
     videoDiv.innerHTML = `
-         <div class="card bg-base-100">
+        <div class="card bg-base-100">
         <figure class="w-full h-50 relative">
-          <img class="w-full h-[180px] object-cover" src="${video.thumbnail} alt="Shoes" />
+          <img class="w-full h-[180px] object-cover" src="${
+            video.thumbnail
+          } alt="Shoes" />
           <span
             class=" absolute bottom-5 right-1 text-sm text-white bg-black opacity-[50%] px-2 rounded"
             >3hrs 56 min ago</span
@@ -84,27 +152,42 @@ const displayVideos = (videos) => {
           </div>
           <div class="intro">
             <h2 class="font-bold">
-              Building a Winning UX Strategy Using the Kano Model
+              ${video.title}
             </h2>
             <p class="text-sm text-gray-400 flex">
               ${video.authors[0].profile_name}
-              <img
-                class="w-5 h-5"
-                src="./assets/icons8-verified-48.png"
-                alt=""
-                srcset=""
-              />
+
+            ${
+              video.authors[0].verified
+                ? `<img
+              class="w-5 h-5"
+              src="./assets/icons8-verified-48.png"
+              alt=""
+              srcset=""
+            />`
+                : ''
+            }
+             
             </p>
             <p class=" text-gray-400 flex">
             ${video.others.views} views
             </p>
           </div>
         </div>
+        <button id="details-${video.video_id}" onclick="loadVideoDetails('${
+      video.video_id
+    }')" class="btn btn-block">Details</button>
       </div>
 
     `;
     videoContainer.appendChild(videoDiv);
   });
 };
+document.getElementById('search-input').addEventListener('keyup', function (e) {
+  const input = e.target.value;
+  // console.log(input);
+  
+    loadVideos(input);
+  
+});
 loadCategories();
-loadVideos();
